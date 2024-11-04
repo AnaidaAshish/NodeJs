cbce2e83a682830f86682979ac32a9d8e9dc8e70
anaidachan <anaidaambilissery@gmail.com>
2024-11-04 08:42:47 +0530

    updates

--- server.js
+++ server.js
@@ -1,21 +1,26 @@
-import express ,{json} from "express";
-import allRoutes from './routes/index.js'
-import dotenv from "dotenv";
-import mongoose from "mongoose";
+import express from "express";
 
-const app = express();
-app.use(express.json())
-dotenv.config()
+import allRoutes from './routes/index.js';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import cookieParser from "cookie-parser";
+const app=express();
+app.use(cookieParser())
+app.use(express.json());
+app.use(cors({origin:"http://localhost:3000",credentials:true}));
+dotenv.config();
 
 app.get("/",(req,res)=>{
     res.send("Home Page");
 });
 
+app.use("/api/v1/", allRoutes)
 
-app.use('/api/v1',allRoutes)
-
-mongoose.connect(process.env.MONGODBURL).then(()=>{console.log("MongoDB connected successfully :)")});
+mongoose.connect(process.env.MONGODBURL).then(()=>{
+    console.log("mongoDb is connected")
+})
 
 app.listen(process.env.PORT,()=>{
-    console.log("The server is running on port 8000")
+    console.log(`server is running on port ${process.env.PORT}`);
 });
