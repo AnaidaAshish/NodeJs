4648d38ca1de67e03624d7581ddae713b40fdb6a
anaidachan <anaidaambilissery@gmail.com>
2024-12-03 18:23:12 +0530

    updates

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ import cookieParser from "cookie-parser";
 const app=express();
 app.use(cookieParser())
 app.use(express.json());
-app.use(cors({origin:["https://products-qsfa.onrender.com", "http://localhost:3000"],credentials:true}));
+app.use(cors({origin:"https://products-qsfa.onrender.com",credentials:true}));
 dotenv.config();
 
 app.get("/",(req,res)=>{
