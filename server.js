93fa9a9ae7dcd595b6892c6e8f70f37b440c9f7a
anaidachan <anaidaambilissery@gmail.com>
2025-02-06 18:48:23 +0530

    updates

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ import cookieParser from "cookie-parser";
 const app=express();
 app.use(cookieParser())
 app.use(express.json());
-app.use(cors({origin:"https://products-dtww.onrender.com",credentials:true}));
+app.use(cors({origin:["https://react-7r85.onrender.com","http://localhost:3000"],credentials:true}));
 dotenv.config();
 
 app.get("/",(req,res)=>{
